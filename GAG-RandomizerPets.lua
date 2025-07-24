984121a5ae97950bf235611c1bc999072e682715
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-24 16:08:47 +0800

    Update GAG-RandomizerPets.lua

--- GAG-RandomizerPets.lua
+++ GAG-RandomizerPets.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2LzjD0nq9zn1xTv7/raw", true))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/gagscript-server2/GAGScript/refs/heads/main/growgarden.lua"))()
+
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2LzjD0nq9zn1xTv7/raw", true))()
